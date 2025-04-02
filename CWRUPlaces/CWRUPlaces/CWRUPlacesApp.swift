f0e262fca2844be8f9da57872009508c2880d144
Amala Penumaka <amalapenumaka26@gmail.com>
2025-04-01 21:06:58 -0400

    created CWRUPlaces

---
+++ CWRUPlaces/CWRUPlaces/CWRUPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWRUPlacesApp.swift
+//  CWRUPlaces
+//
+//  Created by Amala on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CWRUPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
